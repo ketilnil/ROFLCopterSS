6837b174d3687bd754d4b60408b60eff32dbfe23
Ketil Nilsen <ketilnil@gmail.com>
2019-10-03 09:33:11 +0000

    Initial

---
+++ ROFLCopterSS/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
+
+
+namespace ROFLCopterSS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0 || e.Args[0].ToLower().StartsWith("/s"))
+            {
+                foreach (Screen s in Screen.AllScreens)
+                {
+                    if (s != Screen.PrimaryScreen)
+                    {
+                        var window = new Blackout
+                        {
+                            Left = s.WorkingArea.Left,
+                            Top = s.WorkingArea.Top,
+                            Width = s.WorkingArea.Width,
+                            Height = s.WorkingArea.Height
+                        };
+                        window.Show();
+                    }
+                    else
+                    {
+                        var window = new MainWindow
+                        {
+                            Left = s.WorkingArea.Left,
+                            Top = s.WorkingArea.Top,
+                            Width = s.WorkingArea.Width,
+                            Height = s.WorkingArea.Height
+                        };
+                        window.Show();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ROFLCopterSS/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ROFLCopterSS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TransformGroup group = new TransformGroup();
+            double width = this.MainGrid.RenderSize.Width;
+            
+            DoubleAnimation animation = new DoubleAnimation((width / 2) * -1, width / 2 + Roflcopter.ActualWidth, new Duration(new TimeSpan(0, 0, 0, 10)));
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+            TranslateTransform tt = new TranslateTransform(-Roflcopter.ActualWidth * 2, 0);
+            Roflcopter.RenderTransform = group;
+            Roflcopter.Width = 300;
+            Roflcopter.Height = 300;
+            group.Children.Add(tt);
+            tt.BeginAnimation(TranslateTransform.XProperty, animation);
+            WindowState = WindowState.Maximized;
+            Mouse.OverrideCursor = Cursors.None;
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
